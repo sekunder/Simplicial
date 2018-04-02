3fc7b112c27d1db8fc1ab898df29a4e9ff6e1e0a
Alex Kunin <abk170@psu.edu>
2018-04-02 11:57:24 -0400

    pip install not working on osx buildbot, using linux instead

--- docs/make.jl
+++ docs/make.jl
@@ -5,5 +5,5 @@ deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/sekunder/SimplicialDev.jl.git",
     julia  = "0.6",
-    osname = "osx"
+    osname = "linux"
 )
