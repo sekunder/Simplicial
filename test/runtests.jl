24e07fcf730b94737c91efc339cf982f98610002
Alex Kunin <abk170@psu.edu>
2018-04-02 10:22:28 -0400

    simple test, still figuring out Travis-CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,7 @@ C1 = CombinatorialCode([[], [1], [1,2], [2,3]])
 C2 = CombinatorialCode([[], [1],[1,2], [2,3], [2,3]])
 
 @test C1 == C2
+
+L = CombinatorialCode([[1],[3]])
+
+@test link(C1, [2]) == L
